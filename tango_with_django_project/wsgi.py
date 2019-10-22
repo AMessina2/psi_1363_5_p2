e8da1e1dcb13ddb7f562e5283e309e7f17b864a6
AMessina2 <alain.messina@estudiante.uam.es>
2019-10-22 08:19:41 +0200

    Finalizado

--- tango_with_django_project/wsgi.py
+++ tango_with_django_project/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+application = Cling(get_wsgi_application())
 
 application = get_wsgi_application()
